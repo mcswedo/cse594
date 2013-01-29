3f2c513a768a51c020e782bb40c140c32c77a5b6
mcswedo <mcswedo@gmail.com>
2013-01-29 02:21:00 -0800

    Nearly complete re-write of the game. *BROKEN*
    Removed Cell entirely. Cells are used, but as a part of Block.
    
    Rotation is working.
    Movement and screen boundaries are working - a block can now never leave
    the screen, even partially.
    
    However, when we went to re-implement block collision, it gave us some
    major errors. Currently do not know the source or means of fixing.

--- tetris/Block.js
+++ tetris/Block.js
@@ -1,231 +1,225 @@
 //Block.js
-//Original by David Turner, edits and additions by Michael Swedo, Deanna Sulli, and Zac Fuller
+//Original by David Turner, edits and additions by Michael Swedo and Deanna Sulli
 
-function Block() {
-  this.type = 2;
-  this.cells = [];
-  this.posX = 4;
-  this.posY = 0;
-  this.canMoveHorizontal = true;
-  this.rotationNumber = 0;
+function Block(blockType) {
+   this.type = blockType;
    
-  /*Block Type Case Statement.
-    Type === 0 = "I" Block. Rotates from SECOND TO TOP CELL.
-    Type === 1 = "O" Block. Rotates from LOWER LEFT CORNER CELL.
-    Type === 2 = "Z" Block. Rotates from UPPER MIDDLE CELL.
-    Type === 3 = "S" Block. Rotates from UPPER MIDDLE CELL.
-    Type === 4 = "T" Block. Rotates from UPPER MIDDLE CELL.
-    Type === 5 = "L" Block. Rotates from MIDDLE LEFT CELL.
-    Type === 6 = "J" Block. Rotates from MIDDLE RIGHT CELL.
+	this.pos = {
+		x: 4,
+		y: 0
+	}
 
-    LL = lower left, UL = upper left, LR = lower right, UR = upper right, 
-    UM = upper middle, LM = lower middle, ML = middle left, MR = middle right
-  */
-  if (this.type === 0) { //I
-    this.leftEdge = this.posX;    
-    this.rightEdge = this.posX;
-    this.bottomEdge = this.posY - 2;
+	this.r = 0;
+
+   /*Block Type Case Statement.
+      Type === 0 = "LINE" Block. Position Cell is SECOND TO TOP CELL.
+      Type === 1 = "SQUARE" Block. Position is LOWER LEFT CORNER CELL.
+      Type === 2 = "Z" Block. Position Cell is UPPER MIDDLE CELL.
+      Type === 3 = "S" Block. Position Cell is UPPER MIDDLE CELL.
+      Type === 4 = "T" Block. Position Cell is UPPER MIDDLE CELL.
+      Type === 5 = "L" Block. Position Cell is MIDDLE LEFT CELL.
+      Type === 6 = "J" Block. Position Cell is MIDDLE RIGHT CELL.
+
+      Rotation is done hinging around the Position Cell.
+
+      Offset Values are defined in each type for the r = 0 rotation, and are recalculated and adjusted when they need to be.
+   */
+   if (this.type === 0) { //I
+		this.rots = [ 			
+			[ { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: -1 }, { x: 0, y: -2 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: -2, y: 0 }, { x: 1, y: 0 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: -1 }, { x: 0, y: -2 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: -2, y: 0 }, { x: 1, y: 0 } ]
+		]
+
+    this.minXOffset = 0;
+    this.maxXOffset = 0;
+    this.maxYOffset = 1;
+   }
+   if (this.type === 1) { //O
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 1, y: 0 }, { x: 1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 1, y: 0 }, { x: 1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 1, y: 0 }, { x: 1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 1, y: 0 }, { x: 1, y: -1 } ]
+		]
     
-    this.cells.push(new Cell(this.posX, this.posY)); 
-    this.cells.push(new Cell(this.posX, this.posY - 1)); //Highest
-    this.cells.push(new Cell(this.posX, this.posY + 1));
-    this.cells.push(new Cell(this.posX, this.posY + 2)); //Lowest  
-  }
-  if (this.type === 1) { //O
-    this.leftEdge = this.posX;
-    this.rightEdge = this.posX + 1;
-	  this.bottomEdge = this.posY;
-	  
-    this.cells.push(new Cell(this.posX, this.posY)); //LL
-    this.cells.push(new Cell(this.posX, this.posY - 1)); //UL
-    this.cells.push(new Cell(this.posX + 1, this.posY)); //LR
-    this.cells.push(new Cell(this.posX + 1 , this.posY - 1)); //UR
-  }
-  if (this.type === 2) { //Z
-    this.leftEdge = this.posX - 1;
-	  this.rightEdge = this.posX + 1;
-	  this.bottomEdge = this.posY + 1;
-	  
-    this.cells.push(new Cell(this.posX, this.posY)); //UM
-    this.cells.push(new Cell(this.posX, this.posY + 1)); //LM
-    this.cells.push(new Cell(this.posX - 1, this.posY)); //UL
-    this.cells.push(new Cell(this.posX + 1, this.posY + 1)); //LR
-  }
-  if (this.type === 3) { //S
-    this.leftEdge = this.posX - 1;
-	  this.rightEdge = this.posX + 1;
-	  this.bottomEdge = this.posY + 1;
-	  
-    this.cells.push(new Cell(this.posX, this.posY)); //UM
-    this.cells.push(new Cell(this.posX, this.posY + 1)); //LM
-    this.cells.push(new Cell(this.posX + 1, this.posY)); //UR
-    this.cells.push(new Cell(this.posX - 1, this.posY + 1)); //LL
-  }
-  if (this.type === 4) { //T
-    this.leftEdge = this.posX - 1;
-	  this.rightEdge = this.posX + 1;
-	  this.bottomEdge = this.posY + 1;
-	 
-    this.cells.push(new Cell(this.posX, this.posY)); //UM
-    this.cells.push(new Cell(this.posX, this.posY + 1)); //LM
-    this.cells.push(new Cell(this.posX - 1, this.posY)); //UL
-    this.cells.push(new Cell(this.posX + 1, this.posY)); //UR
-  }
-  if (this.type === 5) { //L
-    this.leftEdge = this.posX;
-	  this.rightEdge = this.posX + 1;
-	  this.bottomEdge = this.posY + 1;
-	  
-    this.cells.push(new Cell(this.posX, this.posY)); //ML
-    this.cells.push(new Cell(this.posX, this.posY - 1)); //UL
-    this.cells.push(new Cell(this.posX, this.posY + 1)); //LL
-    this.cells.push(new Cell(this.posX + 1, this.posY + 1)); //LR
-  }
-  if (this.type === 6) { //J
-    this.leftEdge = this.posX - 1;
-	  this.rightEdge = this.posX;
-	  this.bottomEdge = this.posY + 1;
-	  
-    this.cells.push(new Cell(this.posX, this.posY)); //MR
-    this.cells.push(new Cell(this.posX, this.posY - 1)); //UR
-    this.cells.push(new Cell(this.posX, this.posY + 1)); //LR
-    this.cells.push(new Cell(this.posX - 1, this.posY + 1)); //LL
-  }
+    this.minXOffset = 0;
+    this.maxXOffset = 1;
+    this.maxYOffset = 0;
+   }
+   if (this.type === 2) { //Z
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: -1 } ]
+    ]
+
+    this.minXOffset = -1;
+    this.maxXOffset = 1;
+    this.maxYOffset = 1;
+   }
+   if (this.type === 3) { //S
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 1 }, { x: 1, y: 0 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1  }, { x: -1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 1 }, { x: 1, y: 0 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1  }, { x: -1, y: -1 } ]
+		]
+ 
+    this.minXOffset = -1;
+    this.maxXOffset = 1;
+    this.maxYOffset = 1;
+   }
+   if (this.type === 4) { //T
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 1, y: 0 }, { x: 0, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 } ]
+		]
+
+    this.minXOffset = -1;
+    this.maxXOffset = 1;
+    this.maxYOffset = 1;
+   }
+   if (this.type === 5) { //L
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }, { x: 1, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 1, y: 0 }, { x: -1, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }, { x: -1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 1, y: 0 }, { x: 1, y: -1 } ]
+		]
+  
+    this.minXOffset = 0;
+    this.maxXOffset = 1;
+    this.maxYOffset = 1;
+   }
+   if (this.type === 6) { //J
+		this.rots = [
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }, { x: -1, y: 1 } ],
+			[ { x: 0, y: 0 }, { x: 1, y: 0 }, { x: -1, y: 0 }, { x: -1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }, { x: 1, y: -1 } ],
+			[ { x: 0, y: 0 }, { x: -1, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 } ]
+		]
+    
+    this.minXOffset = -1;
+    this.maxXOffset = 0;
+    this.maxYOffset = 1;
+   }
+}
+
+Block.prototype.recalculateOffset = function() {
+  for(var i = 0; i < 4; ++i) { 
+    if(this.rots[this.r][i].x > maxXOffset) {
+      this.maxXOffset = this.rots[this.r][i].x;
+    }
+    if(this.rots[this.r][i].x < minXOffset) {
+      this.minXOffset = this.rots[this.r][i].x;
+    }
+    if(this.rots[this.r][i].y > maxYOffset) {
+      this.maxYOffset = this.rots[this.r][i].y;
+    }
+   }
 };
 
-Block.prototype.draw = function() {
-  for (var i = 0; i < 4; ++i) {
-    this.cells[i].draw();
-  }
+Block.prototype.draw = function(ctx) {
+   for (var i = 0; i < 4; ++i) {
+      drawCell(ctx, this.pos.x + this.rots[this.r][i].x, this.pos.y + this.rots[this.r][i].y);
+   }
 };
 
 Block.prototype.wouldCollide = function(otherBlock) {
-  for (var i = 0; i < 4; ++i) {
-    var cell = this.cells[i];
+  for (var i = 0; i < 4; i++) {
+    var cell = this.rots[this.r][i];
     for (var j = 0; j < 4; ++j) {
-      var otherCell = otherBlock.cells[j];
+      var otherCell = otherBlock.rots[this.r][j];
       if (otherCell.x == cell.x && otherCell.y == cell.y + 1) {
         return true;
-      }
-      if (otherCell.x == cell.x - 1 && otherCell.y == cell.y) {
-        this.canMoveHorizontal = false;
-      }
+      } 
       if (otherCell.x == cell.x + 1 && otherCell.y == cell.y) {
-        this.canMoveHorizontal = false;
+        return true;
+      } 
+      if (otherCell.x == cell.x - 1 && otherCell.y == cell.y) {
+        return true; 
       }
-      }
-   }
-   return false;
-};
-
-Block.prototype.drop = function() {
-   ++this.posY;
-   ++this.bottomEdge;
-   for (var i = 0; i < 4; ++i) this.cells[i].drop();
+    }
+  }
+	return false;
 };
 
 Block.prototype.canDrop = function() {
-   if (this.bottomEdge >= 17) {
-     return false;
-   }
-   for (var i = 0; i < inactiveBlocks.length; ++i) {
-     if (activeBlock.wouldCollide(inactiveBlocks[i])) {
-       return false;
-     }
-   }
-   return true;
-};
+  this.recalculateOffset();
 
-Block.prototype.moveLeft = function() {
-	if(this.canMoveHorizontal == true && this.leftEdge > 0) {
-		--this.posX;
-		--this.leftEdge;
-		--this.rightEdge;
-		for (var i = 0; i < 4; ++i) this.cells[i].moveLeft();
-	}
-};
+  if((this.pos.y + maxYOffset) > 16) {
+    return false;
+  }
 
-Block.prototype.moveRight = function() {
-	if(this.canMoveHorizontal == true && this.rightEdge < 9) {
-		++this.posX;
-		++this.leftEdge;
-		++this.rightEdge;
-		for (var i = 0; i < 4; ++i) this.cells[i].moveRight();
-	}
-};
+  for(var i = 0; i < inactiveBlocks.length; ++i) {
+    if(activeBlock.wouldCollide(inactiveBlocks[i])) {
+      return false;
+    }
+  }
 
-Block.prototype.canRotate = function() {
   return true;
 };
 
-Block.prototype.rotate = function() {
-  if(this.canRotate()) {
-    if(this.type === 0) { //I
-      if(this.rotationNumber === 0) {
-        this.cells = [];
-        this.cells.push(new Cell(this.posX, this.posY)); 
-        this.cells.push(new Cell(this.posX + 1, this.posY)); //Highest
-        this.cells.push(new Cell(this.posX - 1, this.posY));
-        this.cells.push(new Cell(this.posX - 2, this.posY)); //Lowest
-
-        this.leftEdge = this.posX - 2;
-        this.rightEdge = this.posX + 1;
-        this.bottomEdge = this.posY;
-        
-        this.rotationNumber = 0;
-        
-      } else if(this.rotationNumber === 1) {
-        this.cells = [];
-        this.cells.push(new Cell(this.posX, this.posY)); 
-        this.cells.push(new Cell(this.posX, this.posY - 1)); //Highest
-        this.cells.push(new Cell(this.posX, this.posY + 1));
-        this.cells.push(new Cell(this.posX, this.posY + 2)); //Lowest
-
-        this.leftEdge = this.posX;    
-        this.rightEdge = this.posX;
-        this.bottomEdge = this.posY + 2;
-        
-        this.rotationNumber = 1;
-      }
-    }
-  if(this.type === 1) { } //O
-  if(this.type === 2) { //Z
-    if(this.rotationNumber === 0) {
-      this.cells = [];
-      this.cells.push(new Cell(this.posX, this.posY));
-      this.cells.push(new Cell(this.posX - 1, this.posY));
-      this.cells.push(new Cell(this.posX, this.posY - 1));
-      this.cells.push(new Cell(this.posX - 1, this.posY + 1));
-
-      this.leftEdge = this.posX - 1;
-      this.rightEdge = this.posX;
-      this.bottomEdge = this.posY + 1;
-
-      this.rotationNumber = 1;
-    } else if(this.rotationNumber === 1) {
-      this.cells = [];
-      
-      this.cells.push(new Cell(this.posX, this.posY)); //UM
-      this.cells.push(new Cell(this.posX, this.posY + 1)); //LM
-      this.cells.push(new Cell(this.posX - 1, this.posY)); //UL
-      this.cells.push(new Cell(this.posX + 1, this.posY + 1)); //LR
-   
-      this.leftEdge = this.posX - 1;
-	    this.rightEdge = this.posX + 1;
-	    this.bottomEdge = this.posY + 1;
-	    this.rotationNumber = 0;
-    }
-  }
-  if(this.type === 3) { //S
-
-  }
-  if(this.type === 4) { //T
-
-  }
-  if(this.type === 5) { //L
-
-  }
-  if(this.type === 6) { //J 
-
-  } 
+Block.prototype.drop = function() {
+   ++this.pos.y;
 };
+
+Block.prototype.canMoveLeft = function() {
+  this.recalculateOffset();
+
+  if (this.pos.x + minXOffset < 1) {
+    return false;
+  }
+
+  for(var i = 0; i < inactiveBlocks.length; ++i) {
+    if(activeBlock.wouldCollide(inactiveBlocks[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+Block.prototype.moveLeft = function() {
+  --this.pos.x;
+};
+
+Block.prototype.canMoveRight = function() {
+  this.recalculateOffset();
+
+  if(this.pos.x + maxXOffset > 8) {
+    return false;
+  }
+
+  for(var i = 0; i < inactiveBlocks.length; ++i) {
+    if(activeBlock.wouldCollide(inactiveBlocks[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+Block.prototype.moveRight = function() {
+  ++this.pos.x;
+};
+
+Block.prototype.rotate = function() {
+  this.recalculateOffset();
+
+  this.r = ++this.r % 4; //DO THE ROTATION
+  
+  if(this.pos.x + maxXOffset > 8) { //Check to see if the rotation pushed the block off the screen. If so, correct it back in-bounds.
+    this.pos.x = this.pos.x - maxXOffset;
+  }
+  if(this.pos.x + minXOffset < 1) {
+    this.pos.x = this.pos.x - minXOffset;
+  }
+};
+
